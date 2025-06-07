f0346411dcc5f68bb281edf130e55b224b06d1ac
Erik Santillan <santillanerik18@gmail.com>
2025-06-07 16:46:40 -0500

    LibroJava

---
+++ ejerciciosiguiente/ejercicionombre/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public class MiNombre {
+    public static void main(String[] args) {
+        // Declarar la variable nombre y asignarle tu nombre completo
+        String nombre = "ERIK GARCIA";
+
+        // Mostrar el contenido de la variable por pantalla
+        System.out.println("Mi nombre es " + nombre);
+    }
+}
+    }
+
