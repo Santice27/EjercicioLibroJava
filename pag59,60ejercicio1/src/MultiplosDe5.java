f42a54e6ea8b6e52aa92b6a8041819f522493fc2
Erik Santillan <santillanerik18@gmail.com>
2025-06-07 17:53:11 -0500

    LibroJava

---
+++ pag59,60ejercicio1/src/MultiplosDe5.java
@@ -0,0 +1,15 @@
+public class MultiplosDe5 {
+    public static void main(String[] args) {
+        // Recorremos del 0 al 100
+        for (int i = 0; i <= 100; i++) {
+            // Si i es múltiplo de 5
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+        
+    
+
