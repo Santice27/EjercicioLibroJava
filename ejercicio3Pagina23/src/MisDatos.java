64ad03ee0979bfa1e0b20bc0a38da307a46498d7
Erik Santillan <santillanerik18@gmail.com>
2025-06-07 17:08:27 -0500

    LibroJava

---
+++ ejercicio3Pagina23/src/MisDatos.java
@@ -0,0 +1,17 @@
+
+    public class MisDatos {
+    public static void main(String[] args) {
+        // Declaramos las variables
+        String nombre = "Juan Pérez";
+        String direccion = "Calle Solanda, Ciudad Ventanas";
+        String telefono = "0965122115";
+
+        // Mostramos los datos por pantalla, uno en cada línea
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Teléfono: " + telefono);
+
+    }
+    }
+
+
