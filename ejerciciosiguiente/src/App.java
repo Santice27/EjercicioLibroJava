e3f90ddba25cac54285911e88ae3161fa7463d96
Erik Santillan <santillanerik18@gmail.com>
2025-06-07 16:15:34 -0500

    LibroJava

---
+++ ejerciciosiguiente/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public class MisDatos {
+    public static void main(String[] args) {
+        // Mostrar nombre, dirección y teléfono en líneas separadas
+        System.out.println("Nombre: Juan Pérez");
+        System.out.println("Dirección: Calle mariana 123, Ciudad Ventanas");
+        System.out.println("Teléfono: 0986765755");
+    }
+}
+}
