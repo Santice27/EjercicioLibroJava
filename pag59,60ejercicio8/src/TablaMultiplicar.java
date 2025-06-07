e6c8dbadb7ae8f92ab5b530348784b26e03292e6
Erik Santillan <santillanerik18@gmail.com>
2025-06-07 18:00:01 -0500

    LibroJava

---
+++ pag59,60ejercicio8/src/TablaMultiplicar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número para ver su tabla de multiplicar: ");
+        int numero = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+        sc.close();
+    }
+}
+
